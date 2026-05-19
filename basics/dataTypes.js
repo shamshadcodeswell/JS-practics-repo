ee36b634cb69f1be4f6ead3870857b1238dee3db
shamshadcodeswell <shamshadmd2309@gmail.com>
2026-05-19 23:24:08 +0530

    dataTypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,3 @@
+console.log(typeof null); //returns object which is strange about javascript
+//null indicates no value
+console.log(typeof undefined)//returns undefined which indicates value is not yet specified
---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let email = "shamshad1697@gmail.com"
+var accountPassword = "12345"  //dont use var because of block scope and functional scope
+accountCity = "Jaipur" //not preferred
+
+email = "shamshaddmd697@gmail.com"
+accountPassword = 2309
+accountCity = "hyderabad"
+let accountCountry
+// accountId=2
+
+console.log(accountId);
+console.table([accountId, accountPassword, email, accountCity, accountCountry])
