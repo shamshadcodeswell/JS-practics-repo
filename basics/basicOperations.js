a989d9d45835633bbb669244dfa96165b9204627
shamshadcodeswell <shamshadmd2309@gmail.com>
2026-05-20 12:04:49 +0530

    conversions and operations

---
+++ basics/basicOperations.js
@@ -0,0 +1,9 @@
+let value= 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log (2 + 3 + "5" +7 + 8);
+
+let x = "2"
+y = ++x
+console.log(y);
---
+++ basics/dataConversion.js
@@ -0,0 +1,15 @@
+let score = "33a";
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+
+let isLoggedIn = 33;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+let someData = true;
+let stringData = String(someData);
+// console.log(stringData);
+// console.log(typeof stringData);
