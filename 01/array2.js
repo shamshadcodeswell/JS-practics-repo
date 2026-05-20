947751e272a21933a8de3a9c8b9bdd6339afc374
shamshadcodeswell <shamshadmd2309@gmail.com>
2026-05-21 01:20:07 +0530

    arrays 2

---
+++ 01/array2.js
@@ -0,0 +1,23 @@
+let arr1= ["a", "b", "c"];
+let arr2 = ["d", "e", "d"];
+// arr1.push(arr2);
+// console.log(arr1);
+
+// let arr3 = arr1.concat(arr2);
+// console.log(arr3);
+
+// let arr3 = [...arr1 , ...arr2]; //best aproach as it can concat 3 arrays
+// console.log(arr3);
+
+// let nestedArray = [1,2,3,[4,5,6],7,[8,[9,10]]];
+// let denestedArray = nestedArray.flat(Infinity);
+// console.log(denestedArray);
+
+let name = "hitesh";
+let formedArr = Array.from(name);
+console.log(formedArr);
+
+let name = "hitesh";
+console.log(formedArr2);
+
+
