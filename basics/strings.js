b21132898dc905964a38e6ade91fc983e8edabad
shamshadcodeswell <shamshadmd2309@gmail.com>
2026-05-20 19:02:54 +0530

    Strings

---
+++ basics/strings.js
@@ -0,0 +1,27 @@
+const name = new String("Hitesh");
+const repocount = 50;
+// console.log(`hello my name is ${name.toUpperCase()} and my repo count is ${50}`);
+// console.log(name.toUpperCase());
+// console.log(name.charAt(3));
+
+// const newName = name.substring(0,4);
+// console.log(newName);
+
+// const newName2 = name.slice(-3,4);
+// console.log(newName2);
+
+
+// const newName= `   ${name}  `;
+// console.log(newName);
+// console.log(newName.trim());
+
+const url = "https://www.google.com/toy%20of%20rs300only%20rupees";
+const newUrl = url.replaceAll('%20',' ' );
+// console.log(newUrl);
+// console.log(newUrl.includes("google"));
+// console.log(url.split('%20'));
+console.log(url.indexOf("e"));
+
+
+
+ 
