6933d59e3a2e8eda9f1941f5deff0ca99359f241
shamshadcodeswell <shamshadmd2309@gmail.com>
2026-05-21 00:34:42 +0530

    arrays part1

---
+++ 01/array.js
@@ -0,0 +1,39 @@
+let arr = new Array(1, 2, 3);
+
+
+// arr.push("ironman"); //mixing of datatype is allowed + size can expand/shrink
+// arr.pop();
+// console.log(arr);
+// console.log(arr.length);
+
+// arr.unshift(0);   //not preferred as puts excessive load on the system when the array is large
+// console.log(arr);
+
+// arr.shift();
+// console.log(arr);
+
+// console.log(arr.indexOf(4));
+// console.log(arr.includes(4));
+
+
+// console.log(arr);
+// const newArr = arr.join();
+// console.log(newArr);
+
+//slice and splice 
+
+const arr1 = [0,1,2,3,4,5,6];
+// console.log("original array = ",arr1);
+
+const arr2 = arr1.slice(1,5);
+console.log(arr2);
+// console.log("original array = ",arr1);
+
+const arr3 = arr1.splice(1,5);
+console.log(arr3);
+// console.log("original array = ",arr1);
+
+
+
+
+
