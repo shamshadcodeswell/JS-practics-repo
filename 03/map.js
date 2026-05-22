41e8b31231ba2b8cebebdd0d7c4b139a42239c95
shamshadcodeswell <shamshadmd2309@gmail.com>
2026-05-23 00:00:58 +0530

    more array loops :-)

--- 03/map.js
+++ 03/map.js
@@ -1,9 +1,32 @@
 let arr = [1,2,3,4,5,6,7,8,9,10];
 
-const cusArr = arr
-                    .map((item)=> item+10)
-                    .map((item)=> item+1)
-                    .filter((item)=> item%2==0)
+// const cusArr = arr
+//                     .map((item)=> item+10)
+//                     .map((item)=> item+1)
+//                     .filter((item)=> item%2==0)
 
-console.log(cusArr);
+// console.log(cusArr);
 
+// const Total = arr.reduce((acc,cur)=>acc+cur,0);
+// console.log(Total);
+
+
+
+const course = [
+    {
+        course : "JS",
+        price : 399
+    },
+    {
+        course : "mysql",
+        price:499
+    },
+    {
+        course : "react",
+        price : 599
+    }
+]
+
+const cartVal = course.reduce((acc,curr)=>acc+curr.price,0);
+console.log(cartVal);
+ 
